61c52a2b27f1f41a0a2541686cc08cfbb34e2112
“Mr.J” <1157237955@qq.com>
2021-01-05 17:40:44 +0800

    原型模式

---
+++ src/com/designPattern/prototypePattern/PrototypePattern.java
@@ -0,0 +1,23 @@
+package com.designPattern.prototypePattern;
+
+/**
+ * 原型模式
+ */
+public class PrototypePattern {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Realizetype obj1 = new Realizetype();
+        Realizetype obj2 = (Realizetype) obj1.clone();
+        System.out.println("obj1==obj2?" + (obj1 == obj2));
+    }
+
+}
+//具体原型类
+class Realizetype implements Cloneable {
+    Realizetype() {
+        System.out.println("具体原型创建成功！");
+    }
+    public Object clone() throws CloneNotSupportedException {
+        System.out.println("具体原型复制成功！");
+        return (Realizetype) super.clone();
+    }
+}
